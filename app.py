09cb5ade117f041e133b2f0ce36e361470ea21c6
Mohit Dhanik <mohitdhanik010@gmail.com>
2025-06-26 22:16:47 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import pickle
+import nltk
+import string
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+app = Flask(__name__)
+
+# Load the vectorizer and model
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text=text.lower()
+    text=nltk.word_tokenize(text)
+
+    y=[]
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text=y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+    text=y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
+        
+    
+    return " ".join(y)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    if request.method == 'POST':
+        input_mail = request.form['email']
+        transformed_mail = transform_text(input_mail)
+        vector_input = tfidf.transform([transformed_mail])
+        prediction = model.predict(vector_input.toarray())[0]
+
+        if prediction == 1:
+            result = "This is a Spam mail"
+        else:
+            result = "This is a Ham mail"
+
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
